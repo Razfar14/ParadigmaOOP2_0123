048e3d1424e985a1605e2f908a8105f7739d0ccb
Razfar14 <rajarangkuty14@gmail.com>
2026-05-20 18:42:15 +0700

    membuat klass orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class orang{
+public:
+    int umur;
+    orang(int pUmur)  : umur(pUmur)
+    {
+        cout << "Objek orang dibuat dengan umur: " << umur << "\n" << endl;
+    }
+};
 
 
 
