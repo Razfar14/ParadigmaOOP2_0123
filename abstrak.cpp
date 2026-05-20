493c2c31cac74ff63372925da63e5f4cca012b8c
Razfar14 <rajarangkuty14@gmail.com>
2026-05-20 18:39:46 +0700

    membuat kelas abstrak

--- abstrak.cpp
+++ abstrak.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class AbstraksiKlass{
+private:
+    string x, y;
+
+public:
+    //metod untuk mengisi nilai
+    // private member
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+    //meanmpilkan nilai
+    void display(){
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
 
